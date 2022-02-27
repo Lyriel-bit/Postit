08dc97a6d2cde11954419351e179487ffdb3e13d
Lyriel-bit <99033040+Lyriel-bit@users.noreply.github.com>
2022-02-27 23:23:03 +0100

    Update NoteActivity.java

--- PostIt/app/src/main/java/com/example/postit/NoteActivity.java
+++ PostIt/app/src/main/java/com/example/postit/NoteActivity.java
@@ -19,7 +19,25 @@ public class NoteActivity extends AppCompatActivity {
 
         title = findViewById(R.id.id_titleTxtView);
         text = findViewById(R.id.id_noteTextView);
+btnSend = findViewById(R.id.id_buttonsend);
 
+        btnSend.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String mytitle = title.getText().toString();
+                String mynote = textNote.getText().toString();
+
+                Nota nota = new Nota();
+                nota.setTitle(mytitle);
+                nota.setNota(mynote);
+
+                SharedPref.save(getApplicationContext(),nota);
+
+                Intent intent= new Intent(EditNote.this, MainActivity.class);
+                startActivity(intent);
+
+            }
+        }));
 
 
 
